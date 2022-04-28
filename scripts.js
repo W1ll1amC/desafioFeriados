053c94eb4f3bcca92195968e0d5bf6582e7fbab5
William <william.contacto@gmail.com>
2022-04-28 17:09:56 -0400

    primer commit

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@
 //inscrustar en tabla//
 let tableichon = document.getElementById('tablaCuerpo')
 fetch('https://www.feriadosapp.com/api/holidays.json')
-.then(Response => Response.json)
+.then(response => response.json())
 .then(function(data){
-    console.log(data)
+    console.log(data);
     let texto ='';
     texto = data.data.map(e=>{
-        return `<tr><td>${e.id}</`
-        join
+        return `<tr><td>${e.id}</td><td>${e.date}</td><td>${e.title}</td><td>${e.extra}</td></tr>`
 
-    })
-}
+    }).join('');
+    tableichon.innerHTML = texto;
+});
